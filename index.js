fb298e11239fbc55c4054679a975d8dae0a0f16a
Ronald Hilari <ronaldhilari4@gmail.com>
2021-06-29 11:58:01 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+app.listen(3000);
